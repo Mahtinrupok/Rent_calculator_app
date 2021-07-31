5e320bf96965f947b1e6c4a5799d1e85e740ffef
Mahtinrupok <82091449+Mahtinrupok@users.noreply.github.com>
2021-07-31 20:08:44 +0600

    Add files via upload

---
+++ AusRents.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 21:51:19 2020
+
+@author: win10
+"""
+from pydantic import BaseModel
+# 2. Class which describes Bank Notes measurements
+class BankNote(BaseModel):
+    Beds: float 
+    Baths: float 
+    Cars: float 
+    Suburb: str
+    State: str
+    Type: str
+    RentMonth: str
+    
